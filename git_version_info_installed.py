579d433ce288bc48c5026c6bf20482f04b72fba4
Ubuntu <ubuntu@gpu13.maas>
2024-04-17 14:04:16 +0000

    init

---
+++ git_version_info_installed.py
@@ -0,0 +1,6 @@
+version='0.12.6+c00388a2'
+git_hash='c00388a2'
+git_branch='HEAD'
+installed_ops={'async_io': False, 'fused_adam': False, 'cpu_adam': False, 'cpu_adagrad': False, 'cpu_lion': False, 'evoformer_attn': False, 'fused_lamb': False, 'fused_lion': False, 'inference_core_ops': False, 'cutlass_ops': False, 'quantizer': False, 'ragged_device_ops': False, 'ragged_ops': False, 'random_ltd': False, 'sparse_attn': False, 'spatial_inference': False, 'transformer': False, 'stochastic_transformer': False, 'transformer_inference': False}
+compatible_ops={'async_io': False, 'fused_adam': True, 'cpu_adam': True, 'cpu_adagrad': True, 'cpu_lion': True, 'evoformer_attn': False, 'fused_lamb': True, 'fused_lion': True, 'inference_core_ops': True, 'cutlass_ops': True, 'quantizer': True, 'ragged_device_ops': True, 'ragged_ops': True, 'random_ltd': True, 'sparse_attn': False, 'spatial_inference': True, 'transformer': True, 'stochastic_transformer': True, 'transformer_inference': True, 'deepspeed_not_implemented': False}
+torch_info={'version': '2.1', 'bf16_support': False, 'cuda_version': '12.1', 'nccl_version': '2.18', 'hip_version': '0.0'}
